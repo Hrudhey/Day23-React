654d91c9d3fe3d33ae91a718e18385b2b53d5ae6
Hrudhey <hrudhey@gmail.com>
2025-01-22 14:09:52 +0530

    last commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const data = [
         {"NAME":"Palmer","Course":"BTECH","Place":"London","Address":"Address1"},
         {"NAME":"Jennings","Course":"BA","Place":"Pattom","Address":"Address2"},
-        {"NAME":"Jacksin","Course":"BTECH","Place":"Palayam","Address":"Address3"}
+        {"NAME":"Marcus","Course":"BTECH","Place":"Nalanchira","Address":"Address3"}
   ];
 
 const Home = () => {
